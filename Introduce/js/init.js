40134a7d3a19c7480f13aed32c13cf8020baba28
sinee99 <sinee99@naver.com>
2024-04-27 20:10:17 +0900

    10th commit

--- Introduce/js/init.js
+++ Introduce/js/init.js
@@ -5,6 +5,7 @@
 */
 (function($) {
 
+	
 	skel.init({
 		reset: 'full',
 		breakpoints: {
@@ -62,7 +63,46 @@
 				}
 
 			}
+// 이미지 슬라이드
+let imgIndex = 0;
+let position = 0;
+const IMG_WIDTH = 380;
+const $btnPrev = document.querySelector(".btn-prev");
+const $btnNext = document.querySelector(".btn-next");
+const $slideImgs = document.querySelector(".slide-images");
 
+let prev = function () {
+  if (imgIndex > 0) {
+    $btnNext.removeAttribute("disabled");
+    position += IMG_WIDTH;
+    $slideImgs.style.transform = `translateX(${position}px)`;
+    imgIndex = imgIndex - 1;
+  }
+  if (imgIndex == 0) {
+    $btnPrev.setAttribute("disabled", "true");
+  }
+};
+
+let next = function () {
+  if (imgIndex < 5) {
+    $btnPrev.removeAttribute("disabled");
+    position -= IMG_WIDTH;
+    $slideImgs.style.transform = `translateX(${position}px)`;
+    $slideImgs.style.transition = "transform .5s ease-out";
+    imgIndex = imgIndex + 1;
+  }
+  if (imgIndex == 4) {
+    $btnNext.setAttribute("disabled", "true");
+  }
+};
+
+let init = function () {
+  $btnPrev.setAttribute("disabled", "true");
+  $btnPrev.addEventListener("click", prev);
+  $btnNext.addEventListener("click", next);
+};
+
+init();
 		// Dropdowns.
 			$('#nav > ul').dropotron({
 				offsetY: -15,
